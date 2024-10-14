e33625994ac93d83840d8a5fba9c2e9d4732a71e
Omar Jamil <oj249@cam.ac.uk>
2024-10-14 22:35:12 +0100

    Changes for neovim on macs

--- ide_setup.sh
+++ ide_setup.sh
@@ -3,6 +3,17 @@ cd $1
 mkdir software
 mkdir bin
 cd software 
-wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-tar -xzvf nvim-linux64.tar.gz
-ln -s $1/software/nvim-linux64/bin/nvim $1/bin/nvim
+echo "Downloading neovim"
+git clone -b master --single-branch --depth 1 https://github.com/neovim/neovim.git
+cd neovim
+
+make CMAKE_BUILD_TYPE=Release
+rm -r build/  # clear the CMake cache
+make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
+make install
+
+ln -s $HOME/software/neovim/bin/nvim $HOME/bin/nvim
+
+git clone -b main --single-branch https://github.com/omarjamil/ide_config.git
+echo "Follow ide config setup and add neovim to the PATH"
+
