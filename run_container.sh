e4e53f82be1ecfcab8e5b7f432096c910124bb44
Omar Jamil <oj249@cam.ac.uk>
2024-11-02 18:54:12 +0000

    Added options to run containers with and without x forwarding

--- run_container.sh
+++ run_container.sh
@@ -14,10 +14,19 @@ then
   open -a XQuartz
   xhost +
   docker run -it -e DISPLAY=host.docker.internal:0 \
+    --network=host --entrypoint bash --name dev-x \
+    -v /Users/${USER}/Projects:/home/${USER}/Projects \
+    -u $USER dev-oj
+fi
+
+if [[ "$1" == "run-nox" ]]
+then
+  docker run -it \
     --network=host --entrypoint bash --name dev \
     -v /Users/${USER}/Projects:/home/${USER}/Projects \
     -u $USER dev-oj
 fi
+
 
 #-v $COLIMA_SSH_AUTH_SOCK:$COLIMA_SSH_AUTH_SOCK -e SSH_AUTH_SOCK \
 # -v ${SSH_AUTH_SOCK}:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent \
@@ -26,13 +35,27 @@ fi
 
 if [[ "$1" == "start" ]]
 then
+  open -a XQuartz
+  xhost +
+  docker start -ai dev-x
+fi
+
+if [[ "$1" == "start-nox" ]]
+then
   docker start -ai dev
 fi
 
+
 if [[ "$1" == "term" ]]
+then
+  docker exec -ti dev-x bash
+fi
+
+if [[ "$1" == "term-nox" ]]
 then
   docker exec -ti dev bash
 fi
+
 
 # docker compose -f docker-compose.yml build --no-cache
 
