6281d305a002bc8122a2f1d8f4676bb6bb8ecc0e
Omar Jamil <oj249@cam.ac.uk>
2024-12-19 22:33:55 +0000

    missed path requirement

--- ide_setup.sh
+++ ide_setup.sh
@@ -4,7 +4,7 @@ mkdir bin
 cd software 
 wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 tar -xzvf nvim-linux64.tar.gz
-ln -s $1/software/nvim-linux64/bin/nvim $1/bin/nvim
+ln -s $HOME/software/nvim-linux64/bin/nvim $HOME/bin/nvim
 
 git clone -b main --single-branch https://github.com/omarjamil/ide_config.git
 echo "Follow ide config setup and add neovim to the PATH"
